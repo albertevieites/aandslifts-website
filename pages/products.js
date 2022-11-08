75093eb2208dc44b442f2ecc98bc3d00a4d1635b
albertevieites <albertevieites@gmail.com>
2022-11-08 23:15:22 +0000

    Begin Project 🚀

---
+++ pages/products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In laudantium dicta tempore, alias ab nihil excepturi voluptates accusantium corrupti accusamus. Eum asperiores molestiae, facilis sed hic ea quae adipisci eveniet?</p>
+    </div>
+  );
+}
+
+export default Products;
