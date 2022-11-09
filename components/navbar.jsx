f557500753ade01a6612c79a61fc66c220d1855b
albertevieites <albertevieites@gmail.com>
2022-11-09 18:47:34 +0000

    feat: :construction: first structure

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,10 +1,28 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Navbar() {
-  return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/products">Our Products</Link>
-    </div>
-  );
-};
+	return (
+		// NAVBAR
+		<div className='navbar'>
+			<div className='navbar__brand'>
+				<Link href='/'>
+					<h2>A&S</h2>
+				</Link>
+			</div>
+			<nav className='navbar__nav'>
+				<ul className='navbar__list'>
+					<a href='#story'>
+						<li>OUR STORY</li>
+					</a>
+					<a href='#services'>
+						<li>OUR SERVICES</li>
+					</a>
+					<a href='#ftex'>
+						<li>OUR FTEX '22</li>
+					</a>
+				</ul>
+			</nav>
+			<Link href='/products'>Our Products</Link>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,56 @@
-import { gsap } from 'gsap';
-import { useEffect, useRef } from 'react';
+import SocialCard from "../components/socialCard";
 
 export default function Home() {
-	const boxRef = useRef(null);
-
-	useEffect(() => {
-		gsap.to('.box', {
-			rotation: 27,
-			x: 400,
-			duration: 1,
-		});
-	});
 	return (
 		<>
-			<div ref={boxRef} className='box'>
+			{/* HERO SECTION 🚀 */}
+			<section className='hero'>
 				<h1>Leading solutions in lift maintenance</h1>
-			</div>
+				<p>
+					We offer a wide range of products and services to maintain your lift
+					in the best condition.
+				</p>
+			</section>
+
+			{/* PARTNERS SECTION */}
+			<section className='partners'>
+				<h2>NAYAR</h2>
+			</section>
+
+			{/* FEATURES & BENEFITS */}
+			<section className="features">
+				<h2>Features & Benefits</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut minima atque similique natus, nihil neque et molestiae expedita voluptates fugiat earum culpa consectetur dolorum eum optio corrupti exercitationem ipsa tempore.</p>
+			</section>
+
+			{/* SOCIAL PROOF */}
+			<section className="social">
+				<h2>Social Proof</h2>
+				<SocialCard />
+			</section>
+
+			{/* FAQ */}
+			<section className="faq">
+				<h2>FAQ</h2>
+				<h3>Question</h3>
+				<p>Answer: Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sunt hic aspernatur aut molestiae molestias corporis minus quam officia repellat delectus voluptatibus, quibusdam qui error recusandae, fugit doloremque modi totam.</p>
+			</section>
+
+			{/* OUR STORY */}
+			<section className="story">
+				<h2>Our Story</h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam non aspernatur corrupti sed aliquid inventore possimus laboriosam quae, placeat dolorum nulla consequatur facilis officiis veniam harum vitae necessitatibus in quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit non culpa explicabo obcaecati facere, eius, possimus ab reiciendis odio aliquid in eos officiis adipisci animi ipsam soluta voluptatum, reprehenderit iure?</p>
+			</section>
+
+			{/* TEAM */}
+			<section className="team">
+
+			</section>
+
+			{/* LAST */}
+			<section className="last">
+
+			</section>
 		</>
 	);
 }
