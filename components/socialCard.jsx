e879e382ea0eb5fa36dad6f9b5a490d9f0f2aabb
Alberte Vieites <albertevieites@gmail.com>
2022-11-25 19:18:32 +0000

    style: :lipstick: style social and story sections

--- components/socialCard.jsx
+++ components/socialCard.jsx
@@ -1,44 +1,56 @@
-import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 
 import { socialData } from '../data/social';
 
 export default function SocialCard() {
-	const [loading, setLoading] = useState(false);
-
-	// Load this skeleton effect on mount
-	useEffect(() => {
-		setLoading(true);
-		const timer = setTimeout(() => {
-			setLoading(false);
-		}, 5000);
-		// Cancel the timer while unmounting the effect
-		return () => clearTimeout(timer);
-	},[]);
-
 	return (
-		<div className='social__grid'>
+		<>
 			{socialData.map((data, key) => {
 				return (
 					<div key={key} className='social__card'>
-						<p className='social__body'>{loading ? <Skeleton height={120} /> : data.comment}</p>
-						<div className='social__user'>{loading ? <Skeleton width={40} height={40}/> :
-						<Image
-							className='social__image'
-							src={data.img}
-							alt={data.alt}
-							width={50}
-							height={50}
-						/>
-						}
-						<h4 className='social__name'>{loading ? <Skeleton width={228} height={40}/> :data.name}</h4>
+
+						<div className='social__comment'>
+							<Image
+								className='social__quote'
+								src='/../icons/quote.svg'
+								alt='quote'
+								width={20}
+								height={20}
+							/>
+							<p>{data.comment}</p>
 						</div>
+
+						<div className='social__user'>
+							<div className="social__user--persona">
+								<Image
+									className='social__image'
+									src={data.img}
+									alt={data.alt}
+									width={50}
+									height={50}
+								/>
+								<h5 className='social__name'>{data.name}</h5>
+							</div>
+
+							<div className="social__user--social">
+								<Image
+									src={data.socialImg}
+									alt='google icon'
+									width={15}
+									height={15}
+								/>
+								<Image
+									src={data.stars && data.stars}
+									alt='stars'
+									width={60}
+									height={16}
+								/>
+							</div>
+						</div>
+
 					</div>
 				);
 			})}
-		</div>
-	)
+		</>
+	);
 }
